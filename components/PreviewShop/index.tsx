f842f9986261824510cbfbec466aa2bce14f93aa
Nina Vaneeva <ninelamericanel@gmail.com>
2026-01-05 09:18:40 +0400

    added button-link show more and styled

--- components/PreviewShop/index.tsx
+++ components/PreviewShop/index.tsx
@@ -1,5 +1,6 @@
 // серверный компонент, тк получаю данные с сервера по ассортименту магазина
 import { fetchProducts } from "../../api/fetchProducts";
+import Button from "../Button";
 import styles from "./index.module.scss";
 
 export default async function PreviewShop() {
@@ -8,7 +9,7 @@ export default async function PreviewShop() {
     <section className={styles.section}>
       <h2 className={styles.title}> Сладкие моменты</h2>
       <div className={styles.list}></div>
-      <></>
+      <Button type="link" desc="Показать еще"></Button>
     </section>
   );
 }
