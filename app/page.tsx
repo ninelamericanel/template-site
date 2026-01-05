a1ca316326da0127c1e0bc325e385187446e7034
Nina Vaneeva <ninelamericanel@gmail.com>
2026-01-05 09:02:30 +0400

    fixed style scroll for sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import Hero from "../components/Hero/Hero";
 import BannerSlider from "../components/BannerSlider/BannerSlider";
 import Locations from "../components/Locations";
 import Features from "../components/Features";
+import PreviewShop from "../components/PreviewShop";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <BannerSlider />
-      <Locations />
-      <Locations />
-      <Locations />
+      <div>
+        <Locations />
+        <Locations />
+        <Locations />
+      </div>
+      <PreviewShop />
       <Features />
     </>
   );
