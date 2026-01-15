5d343ace02ede4353c35b6dcb10ba534826d5e2a
Nina Vaneeva <ninelamericanel@gmail.com>
2026-01-15 21:43:48 +0400

    refactor component

---
+++ app/utils/findMinMaxShort.ts
@@ -0,0 +1,11 @@
+export function findMinMaxShort(array, fieldName) {
+  if (!array || array.length === 0) return { min: null, max: null };
+
+  const values = array
+    .map((item) => item[fieldName])
+    .filter((value) => typeof value === "number" && !isNaN(value));
+  return {
+    min: Math.min(...values),
+    max: Math.max(...values),
+  };
+}
--- components/FilterComponent/index.tsx
+++ components/FilterComponent/index.tsx
@@ -1,142 +1,25 @@
 "use client";
-
-import { useSelector, useDispatch } from "react-redux";
-import { findMinMaxShort } from "../../src/utils/findMinMaxShort";
-import {
-  ICategory,
-  IFilterState,
-  TCategory,
-  resetFilter,
-  setCategory,
-} from "../../src/state/filterSlice";
 import styles from "./index.module.scss";
-import { RootState } from "../../src/state/store";
 import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import FilterMenu from "../FilterMenu";
+import { useSelector } from "react-redux";
+import { RootState } from "../../src/state/store";
 
-interface ISort {
-  values: TCategory[];
-}
-
-interface IFilterTea {
-  color: "green" | "black";
-  collection: "base" | "holidays" | "special";
-  price: number;
-}
-
-interface IFilterCake {
-  collection: "base" | "holidays" | "special";
-  price: number;
-}
-
-interface IFilterMacarons {
-  collection: "base" | "holidays" | "special";
-  price: number;
-  count: number;
-}
-
-interface IMinMaxVulues {
-  min: null | number;
-  max: null | number;
-}
-
-export const FilterComponent = ({ children }) => {
+export const FilterComponent = () => {
+  const { category } = useSelector((state: RootState) => state.filter);
   const [isOpenFilter, openFilter] = useState<boolean>(false);
-  const { category, subfilters, activeSubfilter, filteredItems } = useSelector(
-    (state: RootState) => state.filter
-  );
-  const dispatch = useDispatch();
-  const filters: ICategory[] = [
-    {
-      value: "all",
-      label: "Все категории",
-    },
-    { value: "cake", label: "Торты" },
-    { value: "chocolate", label: "Шоколад" },
-    { value: "typeTea", label: "Чай" },
-  ];
-
-  const selectTab = (item: ICategory) => {
-    dispatch(setCategory(item));
-  };
-
-  const resetTab = () => {
-    dispatch(resetFilter());
-  };
 
   const openValue = () => {
     openFilter(!isOpenFilter);
   };
 
-  const getPriceInfo = (): IMinMaxVulues => {
-    let obj = { min: null, max: null };
-    obj.min = findMinMaxShort(filteredItems, "price").min;
-    obj.max = findMinMaxShort(filteredItems, "price").max;
-
-    if (obj.max === obj.min) return { min: null, max: obj.max };
-
-    return obj;
-  };
-
-  if (category.value !== "all") {
-    return (
-      <div className={styles.block}>
-        {activeSubfilter.map((item) => {
-          const subfilter = subfilters[item];
-          if (subfilter.name === "price") {
-          }
-
-          return (
-            <>
-              <p className={styles.item} onClick={openValue}>
-                {subfilter.name}
-              </p>
-              {isOpenFilter ? (
-                <div className={styles.subFilter}>
-                  <input
-                    type="range"
-                    min={getPriceInfo().min}
-                    max={getPriceInfo().max}
-                    value={getPriceInfo().min}
-                    // onChange={}
-                    className={styles.input}
-                  />
-                  <div className={styles.values}>
-                    <span>{getPriceInfo().min}</span>
-                    <span>{getPriceInfo().max}</span>
-                  </div>
-                </div>
-              ) : null}
-            </>
-          );
-        })}
-        <p className={styles.back} onClick={resetTab}>
-          &lt;
-        </p>
-      </div>
-    );
-  }
-
-  console.log(subfilters);
-
   return (
     <>
-      <div className={styles.block}>
-        {filters.map((item) => {
-          return (
-            <p className={styles.item} onClick={() => selectTab(item)}>
-              {item.label}
-            </p>
-          );
-        })}
+      <div className={styles.block} onClick={openValue}>
+        <p>{category.label}</p>
       </div>
-      <div className={styles.listAndFilter}>
-        <div className={styles.asideBlock}>
-          {subfilters.map((item) => {
-            return <p>{item.name}</p>;
-          })}
-        </div>
-        {children}
-      </div>
+      <AnimatePresence>{isOpenFilter ? <FilterMenu /> : null}</AnimatePresence>
     </>
   );
 };
