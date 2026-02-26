9736a1b2d7d1773ca16740c3e201f9b77323d90d
Nina Vaneeva <ninelamericanel@gmail.com>
2026-02-26 13:51:01 +0400

    clear BaseInput

--- components/BaseInput/index.tsx
+++ components/BaseInput/index.tsx
@@ -3,6 +3,7 @@ import style from "./index.module.scss";
 import { motion } from "framer-motion";
 import Button from "../Button";
 import { MAX_PERSON, MIN_PERSON } from "../../src/constants";
+import Switcher from "../Switcher";
 
 interface IProps {
   type: string;
@@ -32,111 +33,58 @@ const messageInput = (type) => {
 const BaseInput = ({ type, placeholder, required = false, func, name, pattern }: IProps) => {
   const [isFocused, setFocused] = useState(false);
   const [isError, setError] = useState(false);
-  const [isInfoError, setInfoError] = useState("");
-  const [numberInput, setNumberInput] = useState(MIN_PERSON);
   const alwaysPlaceholder = type === "date" || type === "time";
 
-  console.log(isFocused);
-
-  const countPerson = (action) => {
-    if (action === "-") {
-      if (numberInput >= MIN_PERSON) {
-        setError(false);
-        setNumberInput(numberInput - 1);
-      } else {
-        setError(true);
-      }
-    }
-
-    if (action === "+") {
-      if (numberInput <= MAX_PERSON) {
-        setError(false);
-        setNumberInput(numberInput + 1);
-      } else {
-        setError(true);
-      }
-    }
-  };
-
-  const countInput = (
-    <>
-      <div className={`${style.input} ${style.number}`}>
-        <div className={style.button} onClick={() => countPerson("-")}>
-          <p>-</p>
-        </div>
-
-        <input
-          type={"number"}
-          value={numberInput}
-          min={MIN_PERSON}
-          className={style.numberInput}
-        ></input>
-        <div className={style.button} onClick={() => countPerson("+")}>
-          <p>+</p>
-        </div>
-      </div>
-      {isError || numberInput < MIN_PERSON || numberInput > MAX_PERSON ? (
-        <div className={style.error}>
-          <p>Минимальное количество персон 1, максимально 20</p>
-        </div>
-      ) : null}
-    </>
-  );
+  if (type === "number") return <Switcher />;
 
   return (
     <>
-      {type === "number" ? (
-        countInput
-      ) : (
-        <>
-          <div className={`${style.input} ${isError ? style.inputError : ""}`}>
-            <input
-              name={name}
-              required={required}
-              type={type}
-              placeholder={!isFocused ? placeholder : ""}
-              onFocus={() => setFocused(true)}
-              onBlur={() => setFocused(false)}
-              onChange={(event) => {
-                setError(false);
-                if (event.target.value.match(pattern)) {
-                  func(event.target.value, name);
-                } else {
-                  setError(true);
-                }
-              }}
-            ></input>
-            {isFocused || alwaysPlaceholder ? (
-              <motion.span
-                className={`${style.placeholderMin} ${isError ? style.error : ""}`}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                {placeholder}
-              </motion.span>
-            ) : null}
-          </div>
-          {isError ? (
-            <div className={style.error}>
-              <p>
-                Проверьте правильность введенных данных <span className={style.infoError}>?</span>
-              </p>
+      <div className={`${style.input} ${isError ? style.inputError : ""}`}>
+        <input
+          name={name}
+          required={required}
+          type={type}
+          placeholder={!isFocused ? placeholder : ""}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          onChange={(event) => {
+            setError(false);
+            if (event.target.value.match(pattern)) {
+              func(event.target.value, name);
+            } else {
+              setError(true);
+            }
+          }}
+        ></input>
+        {isFocused || alwaysPlaceholder ? (
+          <motion.span
+            className={`${style.placeholderMin} ${isError ? style.error : ""}`}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            {placeholder}
+          </motion.span>
+        ) : null}
+      </div>
+      {isError ? (
+        <div className={style.error}>
+          <p>
+            Проверьте правильность введенных данных <span className={style.infoError}>?</span>
+          </p>
 
-              {isFocused ? (
-                <motion.span
-                  className={style.tooltip}
-                  initial={{ opacity: 0 }}
-                  exit={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                >
-                  {messageInput(name)}
-                </motion.span>
-              ) : null}
-            </div>
+          {isFocused ? (
+            <motion.span
+              className={style.tooltip}
+              initial={{ opacity: 0 }}
+              exit={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              {messageInput(name)}
+            </motion.span>
           ) : null}
-        </>
-      )}
+        </div>
+      ) : null}
     </>
   );
 };
