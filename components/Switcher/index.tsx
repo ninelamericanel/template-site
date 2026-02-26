475d7ad7af9de22963835dcad222d45e91e9a517
Nina Vaneeva <ninelamericanel@gmail.com>
2026-02-26 13:50:48 +0400

    fixed switcher

--- components/Switcher/index.tsx
+++ components/Switcher/index.tsx
@@ -1,31 +1,22 @@
 import { useState } from "react";
-import { MIN_PERSON } from "../../src/constants";
+import { MAX_PERSON, MIN_PERSON } from "../../src/constants";
+import style from "../Switcher/index.module.scss";
 
 const Switcher = () => {
   const [numberInput, setNumberInput] = useState(MIN_PERSON);
 
   const counter = (action) => {
-    if (action === "-") {
-      if (numberInput >= MIN_PERSON) {
-        setError(false);
-        setNumberInput(numberInput - 1);
-      } else {
-        setError(true);
-      }
+    if (numberInput > MIN_PERSON) {
+      setNumberInput(numberInput - 1);
     }
 
-    if (action === "+") {
-      if (numberInput <= MAX_PERSON) {
-        setError(false);
-        setNumberInput(numberInput + 1);
-      } else {
-        setError(true);
-      }
+    if (numberInput < MAX_PERSON) {
+      setNumberInput(numberInput + 1);
     }
   };
 
   return (
-    <>
+    <div className={style.switcher}>
       <div className={`${style.input} ${style.number}`}>
         <div className={style.button} onClick={() => counter("-")}>
           <p>-</p>
@@ -35,18 +26,15 @@ const Switcher = () => {
           type={"number"}
           value={numberInput}
           min={MIN_PERSON}
+          max={MAX_PERSON}
           className={style.numberInput}
         ></input>
         <div className={style.button} onClick={() => counter("+")}>
           <p>+</p>
         </div>
       </div>
-      {isError || numberInput < MIN_PERSON || numberInput > MAX_PERSON ? (
-        <div className={style.error}>
-          <p>Минимальное количество персон 1, максимально 20</p>
-        </div>
-      ) : null}
-    </>
+      <p className={style.description}>Минимальное количество персон 1, максимально 20</p>
+    </div>
   );
 };
 
